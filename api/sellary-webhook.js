6b6aef9dc8f2a0c013ede7f72bee40c967974ecf
Ankit <starboy00471@gmail.com>
2026-09-16 03:34:13 +0530

    Create sellary-webhook.js

---
+++ api/sellary-webhook.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    console.log("SELLARY WEBHOOK RECEIVED");
+    console.log("Headers:", req.headers);
+    console.log("Body:", req.body);
+
+    return res.status(200).json({
+        success: true,
+        message: "Webhook received"
+    });
+}
